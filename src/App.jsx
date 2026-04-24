1d074c5133425360e6127e80900e2bc56420fd33
rirru <justrirru@icloud.com>
2026-04-24 15:31:35 -0400

    Installed Firebase and added Create function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
+import { collection, addDoc } from "firebase/firestore";
 import React from "react";
+import { db } from "./firebase";
+import { useState } from "react";
 
 const App = () => {
+  const [message, setMessage] = useState("");
+
+  const handleAddPost = async () => {
+    const postRef = collection(db, "posts");
+    await addDoc(postRef, {
+      message,
+    });
+  };
+
   return (
     <main>
       <div>
         <header>
           <h1>React-Firebase Prototype</h1>
+          <div className="flex justify-center mt-8">
+            <input
+              type="text"
+              name="post"
+              id="post"
+              placeholder="Type something..."
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              className="bg-white px-5 border-2 border-blue-300"
+            />
+            <button
+              type="submit"
+              onClick={handleAddPost}
+              className="bg-white px-5 hover:cursor-pointer"
+            >
+              Add post
+            </button>
+          </div>
         </header>
       </div>
     </main>
