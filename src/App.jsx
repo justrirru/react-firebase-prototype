342016512144d36327c54806e4688ccc449ba885
rirru <justrirru@icloud.com>
2026-04-24 16:13:14 -0400

    Added read function

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 import React from "react";
 import { db } from "./firebase";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
   const [message, setMessage] = useState("");
+  const [posts, setPosts] = useState([]);
 
   const handleAddPost = async () => {
-    const postRef = collection(db, "posts");
-    await addDoc(postRef, {
-      message,
-    });
+    try {
+      const postRef = collection(db, "posts");
+      await addDoc(postRef, {
+        message,
+      });
+    } catch (error) {
+      console.error("Error adding post: ", error);
+    }
   };
+
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const postsRef = collection(db, "posts");
+        const postsDoc = await getDocs(postsRef);
+        if (!postsDoc.empty) {
+          const posts = postsDoc.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }));
+          setPosts(posts);
+        }
+      } catch (error) {
+        console.error("Error fetching posts: ", error);
+      }
+    };
+
+    getPosts();
+  }, []);
 
   return (
     <main>
@@ -35,6 +60,16 @@ const App = () => {
             >
               Add post
             </button>
+          </div>
+          <div className="flex flex-col items-center mt-8 text-white">
+            <h2 className="mb-4">Listings</h2>
+            {posts.map((post) => {
+              return (
+                <div key={post.id}>
+                  <h3>{post.message}</h3>
+                </div>
+              );
+            })}
           </div>
         </header>
       </div>
